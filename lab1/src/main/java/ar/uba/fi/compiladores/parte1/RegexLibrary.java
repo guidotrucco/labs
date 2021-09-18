9041a365399a65b92e4a42f663d8265d68fda396
Guido Trucco <gftrucco@gmail.com>
2021-09-18 19:08:15 -0300

    parte 1 ready

--- lab1/src/main/java/ar/uba/fi/compiladores/parte1/RegexLibrary.java
+++ lab1/src/main/java/ar/uba/fi/compiladores/parte1/RegexLibrary.java
@@ -7,24 +7,24 @@ public class RegexLibrary {
      * Regex para decimales que no empiezan en 0
      * @return
      */
-    Pattern getDecimalsRegex(){ return Pattern.compile(""); }
+    Pattern getDecimalsRegex(){ return Pattern.compile("[1-9][0-9]*"); }
 
     /**
      * Regex para decimales que no empiezan en 0
      * @return
      */
-    Pattern getHexaRegex(){ return Pattern.compile(""); }
+    Pattern getHexaRegex(){ return Pattern.compile("[1-9A-F][0-9A-F]*"); }
 
     /**
      * Regex para brainfuck. 
      * @return
      */
-    Pattern getBrainfuckRegex(){ return Pattern.compile(""); }
+    Pattern getBrainfuckRegex(){ return Pattern.compile("[\\+\\-\\[\\]<>]+"); }
 
     /**
      * Nombres: Palabras con mayusculas de al menos 3 letras.
      * @return
      */
-    Pattern getNamesRegex(){ return Pattern.compile(""); }
+    Pattern getNamesRegex(){ return Pattern.compile("[A-Z][a-z]{2,}"); }
 
 }
