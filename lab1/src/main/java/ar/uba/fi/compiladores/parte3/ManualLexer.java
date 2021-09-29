84150c6ebe90470666fbbb150fa66ea321789b00
Guido Trucco <gftrucco@gmail.com>
2021-09-29 19:03:09 -0300

    parte 3 ready

--- lab1/src/main/java/ar/uba/fi/compiladores/parte3/ManualLexer.java
+++ lab1/src/main/java/ar/uba/fi/compiladores/parte3/ManualLexer.java
@@ -1,5 +1,6 @@
 package ar.uba.fi.compiladores.parte3;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,11 +17,39 @@ public class ManualLexer<S,T>{
      * @return
      */
     public SingleTokenExtraction<S> extractToken(String program){
-        this.language.eofTokenType();
-        return null;
+        S currentState = this.language.getInitialState();
+        S lastFinalState = null;
+        int lastFinalStateIndex = 0;
+        for (int i = 0; i < program.length(); i++) {
+            currentState = this.language.transition(currentState, program.charAt(i));
+            if(this.language.getDeadState() == currentState){
+                if (lastFinalState == null) lastFinalState = currentState;
+                return new SingleTokenExtraction(lastFinalState,lastFinalStateIndex);
+            }
+            if(this.language.isFinal(currentState)){
+                lastFinalState=currentState;
+            }
+            lastFinalStateIndex=i;
+        }
+        return new SingleTokenExtraction(lastFinalState,lastFinalStateIndex);
     }
-    public List<Token<T>> lex(String program){
-        return null;
+    public List<Token<T>> lex(String program) throws BadTokenException{
+        List<Token<T>> tokens =  new ArrayList<Token<T>>();
+        SingleTokenExtraction<S> result = this.extractToken(program);
+        while (result.getFinalState() != null){
+            System.out.println(result);
+            if (result.getFinalState()==this.language.getErrorState()){
+                throw new BadTokenException(program.substring(0,result.getLastFinalIndex()+1));
+            }
+            if (result.getFinalState() != this.language.getDeadState()){ 
+                Token<T> t = new Token(this.language.stateToTokenType(result.getFinalState()),program.substring(0,result.getLastFinalIndex()+1));
+                tokens.add(t);
+            }
+            program = program.substring(result.getLastFinalIndex()+1);
+            result = this.extractToken(program);
+        }
+        tokens.add(new Token(this.language.eofTokenType(),null));
+        return tokens;
     }
 
 }
